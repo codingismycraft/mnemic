b74e8a34682d99fccf907f74285c5865c6b6f960
jpazarzis <jpazarzis@rapidsos.com>
2021-05-13 15:54:15 -0400

    Fixes the docker run for postgres.

--- prepare-psql.sh
+++ prepare-psql.sh
@@ -1,2 +1,2 @@
-docker container inspect my-postgres || docker run --name my-postgres -e POSTGRES_PASSWORD=postgres123 5432:5432 -d postgres
+docker container inspect my-postgres || docker run --name my-postgres -e POSTGRES_PASSWORD=postgres123 -p 5432:5432 -d postgres
 docker restart my-postgres
