7bb7584b147680a5d34ed3d063e5419d0fcd96eb
jpazarzis <jpazarzis@rapidsos.com>
2021-05-29 09:41:46 -0400

    Documents utils_impl.py.

--- dolon/impl/utils_impl.py
+++ dolon/impl/utils_impl.py
@@ -1,24 +1,41 @@
-"""Exposes the basic interface to interact with the serialization means."""
+"""Implementation details to interact with the serialization means."""
 
-import datetime
-import io
 import json
 import time
 
-import pandas as pd
-
-import dolon.impl.constants as constants
 import dolon.db_conn as db_conn
+import dolon.impl.constants as constants
 
 # Aliases.
 DbConnection = db_conn.DbConnection
 
 _PREFETCH_SIZE = 100
 
-import logging
-
 
 async def process_message(db, payload):
+    """Processes a tracing message storing it to the db.
+
+    :param db: The database object to use.
+
+    :param dict payload: A dict representing the message to store.
+
+    Can be either a tracing run creation in the form of:
+
+        msg = {
+            "msg_type": "create_trace_run",
+            "app_name": app_name,
+            "uuid": identifier,
+            "column_names": ["v1", 'v2']
+        }
+
+    or for the insertion of a tracing row:
+
+        msg = {
+            "msg_type": "row",
+            "uuid": identifier,
+            "row_data": [12.2, 123.1]
+        }
+    """
     if not isinstance(payload, dict):
         assert isinstance(payload, str)
         msg = json.loads(payload)
@@ -34,75 +51,17 @@ async def process_message(db, payload):
     elif msg_type == 'row':
         identifier = msg.get('uuid')
         row_data = msg.get('row_data')
-        await insert_row(db, identifier, *row_data)
-
-
-async def get_latest_trace(app_name):
-    async with DbConnection() as db:
-        conn_pool = db.get_conn_pool()
-        async with conn_pool.acquire() as conn:
-            stmt = await conn.prepare(constants.SQL_SELECT_LATEST_RUN)
-            async with conn.transaction():
-                async for record in stmt.cursor(app_name,
-                                                prefetch=_PREFETCH_SIZE):
-                    uuid = record['uuid']
-        return await _get_trace(uuid, db)
-
-
-async def get_trace(uuid):
-    async with DbConnection() as db:
-        return await _get_trace(uuid, db)
-
-
-async def _create_tracer(db, identifier, app_name, *column_names):
-    """Creates a new run.
-
-    :param str uuid: The uuid of the run expressed as string.
-    :param str app_name: The application that is been traced.
-    """
-    conn_pool = db.get_conn_pool()
-    async with conn_pool.acquire() as conn:
-        await conn.execute(
-            constants.SQL_INSERT_RUN,
-            identifier,
-            app_name,
-            list(column_names))
-
-
-async def insert_row(db, uuid, *row_data):
-    conn_pool = db.get_conn_pool()
-    async with conn_pool.acquire() as conn:
-        await conn.execute(constants.SQL_INSERT_ROW, uuid, list(row_data))
-
-
-async def _get_trace(uuid, db):
-    conn_pool = db.get_conn_pool()
-    assert uuid
-    assert conn_pool
-    col_names = []
-    async with conn_pool.acquire() as conn:
-        stmt = await conn.prepare(constants.SQL_SELECT_COL_NAMES)
-        async with conn.transaction():
-            async for record in stmt.cursor(uuid, prefetch=_PREFETCH_SIZE):
-                col_names.append(record['col_name'])
-        clauses = [
-            f"row_data[{index + 1}] as {col_name}"
-            for index, col_name in enumerate(col_names)
-        ]
-        sql = "select to_char(date_time, 'YYYY-MM-DD HH24:MI:SS') as timestamp , " + \
-              ','.join(clauses) + \
-              " from tracing_row where uuid=$1 order by 1"
-        stmt = await conn.prepare(sql)
-
-        lines = ['time,' + ','.join(col_names)]
-        async with conn.transaction():
-            async for record in stmt.cursor(uuid, prefetch=_PREFETCH_SIZE):
-                values = [str(v) for v in list(dict(record).values())]
-                lines.append(','.join(values))
-        return '\n'.join(lines)
+        await _insert_row(db, identifier, *row_data)
 
 
 async def get_trace_as_json(uuid):
+    """Returns all the tracing rows for the passed in uuid as json.
+
+    :param str uuid: The identifier for the trace run.
+
+    :returns: All the tracing rows for the passed in uuid as json.
+    :rtype: list[dict]
+    """
     data = await get_trace(uuid)
     lines = data.split('\n')
     field_names = lines[0].split(',')
@@ -124,6 +83,13 @@ async def get_trace_as_json(uuid):
 
 
 async def get_trace_run_info(uuid):
+    """Returns descriptive info for the passed in uuid.
+
+    :param str uuid: The identifier for the trace run.
+
+    :returns: Descriptive info for the passed in uuid.
+    :rtype: dict
+    """
     app_name = None
     counter = None
     from_time = None
@@ -151,5 +117,95 @@ async def get_trace_run_info(uuid):
         'app_name': app_name,
         'counter': counter,
         'started': from_time.strftime("%b %d %Y %H:%M:%S"),
-        'duration': time.strftime("%H hours %M minutes %S seconds", time.gmtime(total_secs))
+        'duration': time.strftime("%H hours %M minutes %S seconds",
+                                  time.gmtime(total_secs))
     }
+
+
+async def get_latest_trace(app_name):
+    """Returns the latest trace for the passed in app_name.
+
+    :return: A list of objects.
+    """
+    async with DbConnection() as db:
+        conn_pool = db.get_conn_pool()
+        async with conn_pool.acquire() as conn:
+            stmt = await conn.prepare(constants.SQL_SELECT_LATEST_RUN)
+            async with conn.transaction():
+                async for record in stmt.cursor(app_name,
+                                                prefetch=_PREFETCH_SIZE):
+                    uuid = record['uuid']
+        return await _get_trace(uuid, db)
+
+
+async def get_trace(uuid):
+    """Returns all the tracing rows for the passed in uuid.
+
+    :param str uuid: The identifier for the trace run.
+
+    :returns: A list of strings representing a csv view of the tracing run.
+    :rtype: list[str]
+    """
+    async with DbConnection() as db:
+        return await _get_trace(uuid, db)
+
+
+async def _create_tracer(db, identifier, app_name, *column_names):
+    """Creates a new run.
+
+    :param str uuid: The uuid of the run expressed as string.
+    :param str app_name: The application that is been traced.
+    """
+    conn_pool = db.get_conn_pool()
+    async with conn_pool.acquire() as conn:
+        await conn.execute(
+            constants.SQL_INSERT_RUN,
+            identifier,
+            app_name,
+            list(column_names))
+
+
+async def _insert_row(db, uuid, *row_data):
+    """Inserts a tracing row to the database.
+
+    :param db: The database object to use.
+    :param str uuid: The identifier for the trace run.
+    :param row_data: Represent the values for each data point.
+    """
+    conn_pool = db.get_conn_pool()
+    async with conn_pool.acquire() as conn:
+        await conn.execute(constants.SQL_INSERT_ROW, uuid, list(row_data))
+
+
+async def _get_trace(uuid, db):
+    """Returns all the tracing rows for the passed in uuid.
+
+    :param str uuid: The identifier for the trace run.
+
+    :returns: A list of strings representing a csv view of the tracing run.
+    :rtype: list[str]
+    """
+    conn_pool = db.get_conn_pool()
+    assert uuid
+    assert conn_pool
+    col_names = []
+    async with conn_pool.acquire() as conn:
+        stmt = await conn.prepare(constants.SQL_SELECT_COL_NAMES)
+        async with conn.transaction():
+            async for record in stmt.cursor(uuid, prefetch=_PREFETCH_SIZE):
+                col_names.append(record['col_name'])
+        clauses = [
+            f"row_data[{index + 1}] as {col_name}"
+            for index, col_name in enumerate(col_names)
+        ]
+        sql = "select to_char(date_time, 'YYYY-MM-DD HH24:MI:SS') as timestamp , " + \
+              ','.join(clauses) + \
+              " from tracing_row where uuid=$1 order by 1"
+        stmt = await conn.prepare(sql)
+
+        lines = ['time,' + ','.join(col_names)]
+        async with conn.transaction():
+            async for record in stmt.cursor(uuid, prefetch=_PREFETCH_SIZE):
+                values = [str(v) for v in list(dict(record).values())]
+                lines.append(','.join(values))
+        return '\n'.join(lines)
