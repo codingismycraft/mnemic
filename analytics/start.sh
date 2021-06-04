507915199484fd71c0a94dff54fe8a8397da87bb
jpazarzis <jpazarzis@rapidsos.com>
2021-06-04 12:34:04 -0400

    Changes analytics start.sh.

--- analytics/start.sh
+++ analytics/start.sh
@@ -1,3 +1,1 @@
-jupyter notebook --ip='*' --port 8888  --NotebookApp.token='' --NotebookApp.password=''
-
 jupyter notebook --ip='*' --port 8888  --NotebookApp.token='' --NotebookApp.password='' --allow-root > error.log &
