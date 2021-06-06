ee3adb21ef13176bc774802e0b881c218de9e5da
jpazarzis <jpazarzis@rapidsos.com>
2021-06-06 11:30:39 -0400

    Adds description for PyPi.

--- setup.py
+++ setup.py
@@ -7,13 +7,20 @@ rm -rf build dist
 sudo python3 setup.py sdist bdist_wheel
 twine upload  --skip-existing dist/* --verbose
 """
+import pathlib
 
 from setuptools import find_packages, setup
+
+_ROOT = pathlib.Path(__file__).parent
+
+with open(str(_ROOT / 'README.rst')) as f:
+    readme = f.read()
 
 setup(
     name="dolon",
     description="A performance tracer application.",
-    version='0.0.42',
+    long_description=readme,
+    version='0.0.44',
     packages=find_packages(),
     install_requires=[
         "asyncpg",
