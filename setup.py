cc719e3356b73281b96de57a9115b3358db9c1a0
jpazarzis <jpazarzis@rapidsos.com>
2021-12-19 15:44:53 -0500

    Upgrades the version to 0.1.1.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     name="dolon",
     description="A performance tracer application.",
     long_description=readme,
-    version='0.0.47',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         "asyncpg",
